f0ea6b4cdb39b7fff5779ad2ee472a06186f9ac4
Heikki Salo <heikki.ao.salo@iki.fi>
2013-05-23 16:28:50 +0300

    currentSHA1 to show SHA1 of given git dir
    Signed-off-by: Heikki Salo <heikki.ao.salo@iki.fi>

--- Documents.hs
+++ Documents.hs
@@ -5,12 +5,28 @@ import System.Directory
 import System.FilePath.Posix
 import Control.Applicative
 import Control.Monad
+import Control.Arrow
+import Lib.Git as G
+
+import Search
+
+
+data Documents = Documents 
 
 load :: FilePath -> IO [Doc]
 load dir = do
    all <- getDirectoryContents dir
+   -- filter out directories (".", "..", ...) from results
    files <- filterM (doesFileExist . toPath) all
    docs <- mapM (readFile . toPath) files
    return docs
   where toPath fn = dir ++ "/" ++ fn
 
+
+-- |Return SHA1 of the current commit in given dir
+currentSHA1 :: FilePath -> IO (Either String String)
+currentSHA1 dir = do
+    out <- G.runGit (G.Config dir Nothing) $ G.gitExec "rev-parse" ["HEAD"] []
+    -- Make possible GitError in Left to String with show
+    return . left show . right (head.lines) $ out
+ 
