d3fa1d1f77fd9e21311f2b40ed64be4b247283c9
Heikki Salo <heikki.ao.salo@iki.fi>
2013-06-01 19:14:48 +0300

    Document memory for Flatbooklet Snaplet
    Signed-off-by: Heikki Salo <heikki.ao.salo@iki.fi>

--- src/Application.hs
+++ src/Application.hs
@@ -10,6 +10,7 @@ module Application where
 import Control.Lens
 import Control.Applicative
 import Control.Monad.State
+import Control.Concurrent.MVar
 import Control.Monad.IO.Class
 import Snap.Snaplet
 import Snap.Snaplet.Heist
@@ -17,10 +18,16 @@ import Snap.Snaplet.Auth
 import Snap.Snaplet.Session
 import Snap.Core
 import Data.Text.Encoding
+import Data.Text
 import Data.Maybe
+import qualified Data.Map as M
 import qualified Data.ByteString as B
 
-data Flatbooklet a = Flatbooklet { flatbookletAuth :: SnapletLens a (AuthManager a) }
+import Types
+
+data Flatbooklet a = Flatbooklet { flatbookletAuth :: SnapletLens a (AuthManager a)
+                                 , userData        :: MVar (M.Map Text [Doc])
+                                 , dataDir         :: FilePath }
 ------------------------------------------------------------------------------
 data App = App
     { _heist :: Snaplet (Heist App)
@@ -44,7 +51,7 @@ deny = do
 
 protect :: Handler a (Flatbooklet a) () -> Handler a (Flatbooklet a) ()
 protect action = (flatbookletAuth <$> get) >>= \auth -> requireUser auth deny $ do
-    login <- (userLogin . fromJust) <$> withTop auth currentUser
+    login <- getUser 
     urlUser <- fmap decodeUtf8 <$> getParam "user"
     case urlUser of
         Nothing -> deny -- invalid URL
@@ -53,15 +60,27 @@ protect action = (flatbookletAuth <$> get) >>= \auth -> requireUser auth deny $ 
                             else deny
 
 getDoc :: Handler a (Flatbooklet a) ()
-getDoc = do
+getDoc = withDocs $ do
     idm <- getParam "id"
     case idm of
         Just ids -> writeBS $ "Document with id: " `B.append` ids
         Nothing ->  do
             modifyResponse $ setResponseStatus 400 "Bad request"
 
+-- |Returns login of the current user logged in.
+-- NOTE: Logged in user is assumed.
+getUser :: Handler a (Flatbooklet a) Text
+getUser = (flatbookletAuth <$> get) >>= \auth -> do
+    (userLogin . fromJust) <$> withTop auth currentUser
+    
+-- |Wrapper that loads documents for current user if needed.
+withDocs :: Handler a (Flatbooklet a) () -> Handler a (Flatbooklet a) ()
+withDocs action = do
+    login <- getUser
+    action
+
 getStats :: Handler a (Flatbooklet a) ()
-getStats = writeText "0 documents"
+getStats = withDocs $ writeText "0 documents"
 
 flatbookletInit :: SnapletLens a (AuthManager a) -> SnapletInit a (Flatbooklet a)
 flatbookletInit authLens = makeSnaplet "Flatbooklet" "Flatbooklet git backend" Nothing $ do
@@ -69,5 +88,7 @@ flatbookletInit authLens = makeSnaplet "Flatbooklet" "Flatbooklet git backend" N
               , ("/:user/get/:id", protect getDoc)
               , ("/:user/stats", protect getStats)
               ]
+    docs <- liftIO $ newMVar M.empty
     liftIO $ putStrLn "Initializing snaplet"
-    return $ Flatbooklet authLens
+    return $ Flatbooklet authLens docs path
+  where path = "data" -- TODO: read from config
