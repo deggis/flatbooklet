3d1bd753ab2927236f62d28f0e62e2bd70c1b373
Heikki Salo <heikki.ao.salo@iki.fi>
2013-06-04 01:56:06 +0300

    Refactor Flatbooklet.hs
    Signed-off-by: Heikki Salo <heikki.ao.salo@iki.fi>

--- src/Snaplet/Flatbooklet.hs
+++ src/Snaplet/Flatbooklet.hs
@@ -41,39 +41,37 @@ protect action = (flatbookletAuth <$> get) >>= \auth -> requireUser auth deny $ 
     urlUser <- fmap decodeUtf8 <$> getParam "user"
     case urlUser of
         Nothing -> deny -- invalid URL
-        Just userName -> if userName == login -- hack attempt
+        Just userName -> if userName == login
                             then action
-                            else deny
+                            else deny -- hack attempt
 
 -- | Serve doc with given id, lookup from user's documents.
 -- Returns HTTP 400 "Bad request" if id missing (routing prevents this)
 -- Returns HTTP 404 "Not found" if document not found
 getDoc :: Handler a (Flatbooklet a) ()
-getDoc = withDocs $ do
+getDoc = do 
     idm <- getParam "id"
+    cache <- readUserCache
     case idm of
-        Just ids -> writeBS $ "Document with id: " `B.append` ids
-        Nothing ->  do
-            modifyResponse $ setResponseStatus 400 "Bad request"
+        Just ids -> writeBS $ "TODO: Document with id: " `B.append` ids
+        Nothing  -> modifyResponse $ setResponseStatus 400 "Bad request"
 
 -- | Serve all user's documents in JSON list containing
 -- { id: ID, doc: DOC } doc ids and documents truncated
 -- to 100 characters.
-getDocs :: Handler a (Flatbooklet a) [Doc]
+getDocs :: Handler a (Flatbooklet a) ()
 getDocs = do
-    login <- getLogin
-    datavar <- userData <$> get
-    d <- M.lookup login <$> (liftIO $ readTVarIO datavar)
-    case d of
-        Just docs -> return docs
-        Nothing   -> return ["error"]
+    cache <- readUserCache
+    -- FIXME: format as specs say
+    writeText . T.pack . show $ cache
+
 
 -- | Return user document overall statistics as JSON
 -- document.
 getStats :: Handler a (Flatbooklet a) ()
-getStats = withDocs $ do
-    docs <- getDocs
-    writeText . T.pack $ show (length docs) ++ " documents"
+getStats = undefined --withDocs -- $ d
+--    docs <- getDocs
+--    writeText . T.pack $ show (length docs) ++ " documents"
 
 
 -- | Returns login of the current user logged in.
@@ -82,11 +80,12 @@ getLogin :: Handler a (Flatbooklet a) Text
 getLogin = (flatbookletAuth <$> get) >>= \auth -> do
     (userLogin . fromJust) <$> withTop auth currentUser
     
--- |Wrapper that loads documents for current user if needed.
--- FIXME: simply do this only at user login
--- FIXME: rename
-withDocs :: Handler a (Flatbooklet a) () -> Handler a (Flatbooklet a) ()
-withDocs action = do
+
+
+-- | Perform tasks associated to user login. Load documents and
+-- create indices.
+atLogin :: Handler a (Flatbooklet a) ()
+atLogin = do 
     login <- getLogin
     datavar <- userData <$> get
     liftIO . atomically $ do
@@ -94,20 +93,45 @@ withDocs action = do
         if M.member login userData
             then return ()
             else modifyTVar' datavar (\m -> M.insert login ["sampledata","asd"] m)
-    action     
 
-atLogin :: Handler a (Flatbooklet a) ()
-atLogin = liftIO $ putStrLn "FB: login"
-
+-- | Perform tasks associated to user logout. Forget all kept stuff.
 atLogout :: Handler a (Flatbooklet a) ()
-atLogout = liftIO $ putStrLn "FB: logout"
+atLogout = rmUserCache
 
 flatbookletInit :: SnapletLens a (AuthManager a) -> SnapletInit a (Flatbooklet a)
 flatbookletInit authLens = makeSnaplet "Flatbooklet" "Flatbooklet git backend" Nothing $ do
     addRoutes [ ("/hello", writeText "hello world with snaplets!" )
               , ("/:user/get/:id", protect getDoc)
-              , ("/:user/stats", protect getStats)
+              , ("/:user/all",     protect getDocs)
+              , ("/:user/stats",   protect getStats)
               ]
     docs <- liftIO $ newTVarIO M.empty
     return $ Flatbooklet authLens docs path
   where path = "data" -- TODO: read from config
+
+
+
+
+-- cache poking helpers
+        
+readUserCache = do
+    (l,v) <- loginAndCacheTVar
+    cache <- liftIO $ readTVarIO v
+    return . fromJust . M.lookup l $ cache
+
+withUserCache action = do
+    (l,v) <- loginAndCacheTVar
+    liftIO . atomically $ do
+        cache <- readTVar v
+        let userdocs = fromMaybe [] $ M.lookup l cache
+        -- Data.Map.insert adds or replaces (if needed) previous contents
+        modifyTVar' v $ M.insert l (action userdocs)
+
+rmUserCache = do
+    (l,v) <- loginAndCacheTVar
+    liftIO . atomically $ modifyTVar' v (\m -> M.delete l m)
+
+loginAndCacheTVar = do
+    login <- getLogin
+    cvar <- userData <$> get
+    return (login,cvar)
